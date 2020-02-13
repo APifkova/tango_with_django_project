96d2b212839b09e19ebeb8f8358592987cb690c5
Andrea Pifkova <2403464p@student.gla.ac.uk>
2020-02-13 22:20:44 +0000

    Chapter 10 finished

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -121,10 +121,7 @@ USE_TZ = True
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
-
 LOGIN_URL = 'rango:login'
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
