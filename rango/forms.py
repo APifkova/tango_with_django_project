4efebedc3f8aae2912474f04704d70c19e384224
Andrea Pifkova <2403464p@student.gla.ac.uk>
2020-02-13 19:16:42 +0000

    Chapter 9 progress

--- rango/forms.py
+++ rango/forms.py
@@ -33,7 +33,7 @@ class PageForm(forms.ModelForm):
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget.forms.PasswordInput())
+    password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
